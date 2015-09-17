7024f7b99191fbbe8d433014b2fa9af983b80c02
Pterolatypus <pterolatypus@gmail.com>
2015-09-17 10:20:15 +0100

    Added copyright/license notice to Rift.java

--- src/main/java/com/github/pterolatypus/rift/Rift.java
+++ src/main/java/com/github/pterolatypus/rift/Rift.java
@@ -1,5 +1,11 @@
 package com.github.pterolatypus.rift;
 
+/**
+ * Rift is a modification for Minecraft, built against Minecraft Forge. Copyright (C) 2015 Pterolatypus
+ * This program is free software: you can redistribute it and/or modify it under the terms of version 2 of the GNU General Public License as published by the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ */
+
 import com.github.pterolatypus.rift.proxy.IProxy;
 import com.github.pterolatypus.rift.reference.Strings;
 import net.minecraft.init.Blocks;
